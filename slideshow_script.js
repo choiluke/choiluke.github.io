5c3ff57d2f6d14529602b81cbbd1e2577d9916df
franchester4 <lukee.choi21@gmail.com>
2023-07-29 23:04:33 -0400

    fix slideshow

--- slideshow_script.js
+++ slideshow_script.js
@@ -1,14 +1,14 @@
 var curr = 0;
+var img_arr = document.getElementsByClassName("slideshow");
 
 auto_next();
-var img_arr = document.getElementsByClassName("slideshow");
 
 function autonext() {
   var iter;
   for (iter = 0; iter < img_arr.length; iter++) {
-    x[iter].style.display = "none";
+    img_arr[iter].style.display = "none";
   }
-  x[curr].style.display = "block";
+  img_arr[curr].style.display = "block";
   curr = (curr + 1) % img_arr.length;
   setTimeout(autonext, 4000);
 }
